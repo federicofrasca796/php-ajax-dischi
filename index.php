0d36249eec80660a5711099a528f0991649467be
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-12-09 14:30:54 +0100

    add html structure | link css, js, axios, vue

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Dischi</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div id="app">
+        <header></header>
+        <main></main>
+    </div>
+
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="./assets/js/app.js"></script>
+</body>
+
+</html>
